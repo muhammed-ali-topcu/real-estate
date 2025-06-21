d466ba37b1e92b3f0daaf3a62fdd399e16cdd346
Muhammed Ali 01 <m.aligenel01@gmail.com>
2025-06-21 12:59:17 +0300

    add turkiye seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(TurkiyeCiteiesSeeder::class);
         $this->call(PropertySeeder::class);
 
     }
