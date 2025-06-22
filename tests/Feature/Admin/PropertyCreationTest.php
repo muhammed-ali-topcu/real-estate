28ea3120ce23c0009ca3d96d347030504d6f0f33
Muhammed Ali 01 <m.aligenel01@gmail.com>
2025-06-22 12:49:38 +0300

    add property creating test

---
+++ tests/Feature/Admin/PropertyCreationTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Enums\PropertyListingTypes;
+use App\Enums\PropertyRooms;
+use App\Enums\PropertyStatuses;
+use App\Enums\PropertyTypes;
+use App\Enums\RolesEnum;
+use App\Models\City;
+use App\Models\Country;
+use App\Models\District;
+use App\Models\User;
+use Database\Seeders\RolesSeeder;
+
+test('admin can create a property', function () {
+    // Create necessary related models
+    $country  = Country::factory()->create();
+    $city     = City::factory()->create(['country_id' => $country->id]);
+    $district = District::factory()->create(['city_id' => $city->id]);
+
+    $this->seed(RolesSeeder::class);
+    $admin = User::factory()->create();
+    $admin->assignRole(RolesEnum::ADMIN->value);
+
+    // Visit the create property page
+    $response = $this->actingAs($admin)
+        ->get(route('admin.properties.create'));
+
+    // Assert we can see the create property page
+    $response->assertStatus(200);
+
+    // Create a property
+    $payload = [
+        'title'         => 'Test Property',
+        'description'   => 'This is a test property',
+        'address'       => '123 Test Street',
+        'property_type' => PropertyTypes::APARTMENT->value,
+        'listing_type'  => PropertyListingTypes::SALE->value,
+        'price'         => 100000,
+        'rooms'         => PropertyRooms::ROOM_1_PLUS_1->value,
+        'area'          => 100,
+        'status'        => PropertyStatuses::PENDING->value,
+        'country_id'    => $country->id,
+        'city_id'       => $city->id,
+        'district_id'   => $district->id,
+    ];
+
+    $response = $this->actingAs($admin)
+        ->post(route('admin.properties.store'), $payload);
+
+    //Assert we were redirected to the properties index
+    $response->assertRedirect(route('admin.properties.index'));
+
+    // Assert the property was created in the database
+    $this->assertDatabaseHas('properties', $payload);
+});
+
+test('property creation fails with invalid data', function () {
+    $this->seed(RolesSeeder::class);
+    $admin = User::factory()->create();
+    $admin->assignRole(RolesEnum::ADMIN->value);
+
+    $payload = [
+        'title'         => 'Test Property',
+        'description'   => 'This is a test property',
+        'address'       => '123 Test Street',
+        'property_type' => 'sss',
+        'listing_type'  => 'sssfdsfs',
+        'price'         => -4654,
+        'rooms'         => 'sss flsdkjf lksd',
+        'area'          => 'jflds',
+        'status'        => 'sss',
+        
+    ];
+
+    // Try to create a property with invalid data
+    $response = $this->actingAs($admin)
+        ->post(route('admin.properties.store'), $payload);
+
+    // Assert we see validation errors
+    $response->assertSessionHasErrors([
+        'price',
+        'area',
+        'country_id',
+        'city_id',
+        'district_id',
+        'property_type',
+        'listing_type',
+        'rooms',
+        'status',
+    ]);
+});
